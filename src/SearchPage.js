87e42368110ff0a52958f1d4a45c9cf146adbafc
Fernanda Roxo <fernandaroxo@gmail.com>
2018-07-12 18:37:00 +0100

    Create list and search components

---
+++ src/SearchPage.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+
+class SearchPage extends Component {
+	state = {
+		query: ''
+	}
+
+	updateQuery = (query) => {
+		this.setState({query: query.trim()})
+	}
+
+	clearQuery = () => {
+		this.setState({query: ''})
+	}
+
+	render() {
+		const {books} = this.props
+		const {query} = this.state
+
+		let showingBooks = books
+		if (query) {
+			const match = new RegExp(escapeRegExp(query), 'i')
+			showingBooks = books.filter((book) => match.test(book.name))
+			console.log(showingBooks)
+		}
+
+		return (
+
+			<div className="search-books">
+				<div className="search-books-bar">
+					<a className="close-search" onClick={() => this.setState({ showSearchPagePage: false })}>Close</a>
+					<div className="search-books-input-wrapper">
+						{/*
+							NOTES: The search from BooksAPI is limited to a particular set of search terms.
+							You can find these search terms here:
+							https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+							However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+							you don't find a specific author or title. Every search is limited by search terms.
+						*/}
+						<input
+							type="text"
+							placeholder="SearchPage by title or author"
+							value={query}
+							onChange={(event) => this.updateQuery(event.target.value)}
+						/>
+
+					</div>
+				</div>
+				<div className="search-books-results">
+					<ol className="books-grid"></ol>
+				</div>
+
+				<div className='list-books-top'>
+
+				</div>
+
+				<ol className='book-list'>
+					{showingBooks.map((book) => (
+						<li key={book.id} className='book-list-item'>
+						 <div className='book-avatar' style={{
+							 backgroundImage: `url(${book.avatarURL})`
+						 }}/>
+						 <div className='book-details'>
+							<p>My book</p>
+						 </div>
+						</li>
+					))}
+				</ol>
+			</div>
+		)
+	}
+}
+
+export default SearchPage
